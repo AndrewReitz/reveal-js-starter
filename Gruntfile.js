2c2dc171162369e7a48d247bd0e6bced2b04afcd
Andrew Reitz <aj.reitz@gmail.com>
2014-08-04 13:20:33 -0500

    Updated to have release tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,46 @@ module.exports = function (grunt) {
                     }
                 ]
             }
+        },
+        clean: {
+            build: [
+                "dist/css",
+                "dist/fonts",
+                "dist/images",
+                "dist/lib",
+                "dist/index.html"
+            ]
+        },
+        copy: {
+            release: {
+                expand: true,
+                cwd: 'src/',
+                src: '**',
+                dest: 'dist/'
+            },
+            releaseReplace: { // Done this way instead of with regular copy is it screws up the image files that are being copied over
+                src: 'dist/index.html',
+                dest: 'dist/index.html',
+                options: {
+                    process: function (content, srcpath) { // Remove remote from release/gh-pages version
+                        return content.replace(
+                            "{ src: 'lib/reveal.js/plugin/remotes/remotes.js', async: true, condition: function () { return !!document.body.classList; }}",
+                            ""
+                        );
+                    }
+                }
+            }
+        },
+        'gh-pages': {
+            target: {
+                options: {
+                    // The default commit message for the gh-pages branch
+                    message: 'pushing updates',
+                    base: 'dist'
+                },
+                // The folder where your gh-pages repo is
+                src: '**/*'
+            }
         }
     });
 
@@ -69,6 +109,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-html');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-gh-pages');
 
 
     // Default task
@@ -76,13 +119,26 @@ module.exports = function (grunt) {
 
     grunt.registerTask('help', 'Prints out the specific details of each command', function () {
         console.log("Available Commands");
+        console.log("  dev:\tRuns all dev commands, if your are developing you want this");
+        console.log("  release:\tRuns all releases commands putting content in the dist folder");
+        console.log("  deploy:\tRuns release, and pushes content to GitHub pages");
         console.log("  watch:\tAuto pushes updates to the browser on save");
         console.log("  connect:\tStart a local server for testing on port 8888");
-        console.log("  dev:\tRuns all dev commands, if your are developing you want this");
         console.log("  open:connect\tOpens Google Chrome to the index page on port 8888");
-        console.log("  open:dev\tOpens Google Chrome to the html file in chrome");
+        console.log("  open:dev\tOpens the html file in chrome");
         console.log("  imagemin\tOptimize your images for the web");
     });
 
     grunt.registerTask('dev', 'Runs all dev commands, if your are developing you want this', 'concurrent:target');
+
+    grunt.registerTask('release', 'Creates the release build and publishes it to github pages',
+        [
+            'clean:build',
+            'imagemin',
+            'copy:release',
+            'copy:releaseReplace'
+        ]
+    );
+
+    grunt.registerTask('deploy', ['release', 'gh-pages:target']);
 };
